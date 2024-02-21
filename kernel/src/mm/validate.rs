fffcefff6277e97020a32e71dcc0ca649df248b7
Chuanxiao Dong <chuanxiao.dong@intel.com>
2024-02-21 15:41:34 +0800

    kernel/mm/validate: Extend ValidBitmap to use PageRef
    ValidBitmap currently is used as bitmap for SVSM memory. When it is used
    for guest memory, then the bitmap memory size may be larger than the
    maximum memory size allocate_pages() can support. To resolve this, use
    PageRef as an altertative to store the bitmap. So ValidBitmap can
    support using either *mut u64 or Vec<PageRef>. With this change, the
    stage2 still init VALID_BITMAP with *mut u64, and svsm will migrate the
    bitmap contents from *mut u64 to Vec<PageRef>.
    
    Co-developed-by: Peter Fang <peter.fang@intel.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- kernel/src/mm/validate.rs
+++ kernel/src/mm/validate.rs
@@ -4,48 +4,56 @@
 //
 // Author: Joerg Roedel <jroedel@suse.de>
 
+extern crate alloc;
+
 use crate::address::{Address, PhysAddr, VirtAddr};
 use crate::error::SvsmError;
 use crate::locking::SpinLock;
-use crate::mm::alloc::{allocate_pages, get_order};
-use crate::mm::virt_to_phys;
+use crate::mm::alloc::{allocate_file_page_ref, allocate_pages, get_order};
+use crate::mm::{virt_to_phys, PageRef};
 use crate::types::{PAGE_SIZE, PAGE_SIZE_2M};
-use crate::utils::MemoryRegion;
+use crate::utils::{page_align_up, MemoryRegion};
+use alloc::vec::Vec;
+use core::cmp::min;
 use core::ptr;
 
 static VALID_BITMAP: SpinLock<ValidBitmap> = SpinLock::new(ValidBitmap::new());
+// Number of u64's in one page
+const PAGE_N64: usize = PAGE_SIZE / core::mem::size_of::<u64>();
 
 #[inline(always)]
-fn bitmap_alloc_order(region: MemoryRegion<PhysAddr>) -> usize {
-    let mem_size = region.len() / (PAGE_SIZE * 8);
-    get_order(mem_size)
+fn bitmap_bytes(region: MemoryRegion<PhysAddr>) -> usize {
+    page_align_up(region.len().div_ceil(PAGE_SIZE).div_ceil(u8::BITS as usize))
 }
 
 pub fn init_valid_bitmap_ptr(region: MemoryRegion<PhysAddr>, bitmap: *mut u64) {
     let mut vb_ref = VALID_BITMAP.lock();
     vb_ref.set_region(region);
-    vb_ref.set_bitmap(bitmap);
+    vb_ref.set_bitmap_ptr(bitmap);
 }
 
 pub fn init_valid_bitmap_alloc(region: MemoryRegion<PhysAddr>) -> Result<(), SvsmError> {
-    let order: usize = bitmap_alloc_order(region);
+    let order: usize = get_order(bitmap_bytes(region));
     let bitmap_addr = allocate_pages(order)?;
 
     let mut vb_ref = VALID_BITMAP.lock();
     vb_ref.set_region(region);
-    vb_ref.set_bitmap(bitmap_addr.as_mut_ptr::<u64>());
+    vb_ref.set_bitmap_ptr(bitmap_addr.as_mut_ptr::<u64>());
     vb_ref.clear_all();
 
     Ok(())
 }
 
 pub fn migrate_valid_bitmap() -> Result<(), SvsmError> {
-    let order: usize = VALID_BITMAP.lock().alloc_order();
-    let bitmap_addr = allocate_pages(order)?;
+    let mut page_refs = Vec::new();
+    let page_count = VALID_BITMAP.lock().bitmap_npages();
+    for _ in 0..page_count {
+        page_refs.push(allocate_file_page_ref()?);
+    }
 
     // lock again here because allocator path also takes VALID_BITMAP.lock()
     let mut vb_ref = VALID_BITMAP.lock();
-    vb_ref.migrate(bitmap_addr.as_mut_ptr::<u64>());
+    vb_ref.migrate(page_refs);
     Ok(())
 }
 
@@ -95,16 +103,22 @@ pub fn valid_bitmap_valid_addr(paddr: PhysAddr) -> bool {
 }
 
 #[derive(Debug)]
+enum ValidBitmapMem {
+    Ptr(*mut u64),
+    PageRefs(Vec<PageRef>),
+}
+
+#[derive(Debug)]
 struct ValidBitmap {
     region: MemoryRegion<PhysAddr>,
-    bitmap: *mut u64,
+    bitmap: Option<ValidBitmapMem>,
 }
 
 impl ValidBitmap {
     const fn new() -> Self {
         ValidBitmap {
             region: MemoryRegion::from_addresses(PhysAddr::null(), PhysAddr::null()),
-            bitmap: ptr::null_mut(),
+            bitmap: None,
         }
     }
 
@@ -112,8 +126,18 @@ impl ValidBitmap {
         self.region = region;
     }
 
-    fn set_bitmap(&mut self, bitmap: *mut u64) {
-        self.bitmap = bitmap;
+    fn set_bitmap_ptr(&mut self, bitmap: *mut u64) {
+        if bitmap.is_null() {
+            return;
+        }
+        self.bitmap = Some(ValidBitmapMem::Ptr(bitmap));
+    }
+
+    fn set_bitmap_page_refs(&mut self, page_refs: Vec<PageRef>) {
+        if page_refs.is_empty() {
+            return;
+        }
+        self.bitmap = Some(ValidBitmapMem::PageRefs(page_refs));
     }
 
     fn check_addr(&self, paddr: PhysAddr) -> bool {
@@ -121,46 +145,135 @@ impl ValidBitmap {
     }
 
     fn bitmap_addr(&self) -> PhysAddr {
-        assert!(!self.bitmap.is_null());
-        virt_to_phys(VirtAddr::from(self.bitmap))
+        assert!(self.bitmap.is_some());
+        let Some(bitmap) = &self.bitmap else {
+            panic!("ValidBitmap bitmap is none");
+        };
+
+        match bitmap {
+            ValidBitmapMem::Ptr(ptr) => virt_to_phys(VirtAddr::from(*ptr)),
+            _ => PhysAddr::null(),
+        }
     }
 
     #[inline(always)]
     fn index(&self, paddr: PhysAddr) -> (usize, usize) {
         let page_offset = (paddr - self.region.start()) / PAGE_SIZE;
-        let index = page_offset / 64;
-        let bit = page_offset % 64;
+        let index = page_offset / u64::BITS as usize;
+        let bit = page_offset % u64::BITS as usize;
 
         (index, bit)
     }
 
     fn clear_all(&mut self) {
         let len = self.bitmap_len();
-        unsafe {
-            ptr::write_bytes(self.bitmap, 0, len);
+
+        if let Some(bitmap) = &mut self.bitmap {
+            match bitmap {
+                ValidBitmapMem::Ptr(ptr) => unsafe {
+                    ptr::write_bytes(*ptr, 0, len);
+                },
+                ValidBitmapMem::PageRefs(pr) => {
+                    for page_ref in pr.iter_mut() {
+                        unsafe {
+                            let ptr = page_ref.as_mut() as *mut [u8; PAGE_SIZE] as *mut u64;
+                            ptr::write_bytes(ptr, 0, PAGE_N64);
+                        }
+                    }
+                }
+            }
         }
     }
 
-    fn alloc_order(&self) -> usize {
-        bitmap_alloc_order(self.region)
+    fn bitmap_npages(&self) -> usize {
+        bitmap_bytes(self.region) / PAGE_SIZE
     }
 
     /// The number of u64's in the bitmap
     fn bitmap_len(&self) -> usize {
-        let num_pages = self.region.len() / PAGE_SIZE;
-        num_pages.div_ceil(u64::BITS as usize)
+        self.bitmap_npages() * PAGE_N64
     }
 
-    fn migrate(&mut self, new_bitmap: *mut u64) {
-        let count = self.bitmap_len();
-        unsafe {
-            ptr::copy_nonoverlapping(self.bitmap, new_bitmap, count);
+    fn migrate(&mut self, mut page_refs: Vec<PageRef>) {
+        let mut count = self.bitmap_len();
+        if let Some(bitmap) = &mut self.bitmap {
+            match bitmap {
+                ValidBitmapMem::Ptr(ptr) => {
+                    for (i, page_ref) in page_refs.iter_mut().enumerate() {
+                        let src = ptr.wrapping_add(i * PAGE_N64);
+                        let len = min(count, PAGE_N64);
+                        unsafe {
+                            let dst = page_ref.as_mut() as *mut [u8; PAGE_SIZE] as *mut u64;
+                            if len > 0 {
+                                ptr::copy_nonoverlapping(src, dst, len);
+                            }
+                            // Zero out any unused u64's
+                            if len < PAGE_N64 {
+                                let dst = dst.wrapping_add(len);
+                                ptr::write_bytes(dst, 0, PAGE_N64 - len);
+                            }
+                        }
+                        count -= len;
+                    }
+                }
+                ValidBitmapMem::PageRefs(pr) => {
+                    let src_pr = pr.as_slice();
+                    let dst_pr = page_refs.as_mut_slice();
+
+                    for i in 0..pr.len() {
+                        unsafe {
+                            let src = src_pr[i].as_ref() as *const [u8; PAGE_SIZE] as *const u64;
+                            let dst = dst_pr[i].as_mut() as *mut [u8; PAGE_SIZE] as *mut u64;
+                            ptr::copy_nonoverlapping(src, dst, PAGE_N64);
+                        }
+                    }
+                }
+            }
         }
-        self.bitmap = new_bitmap;
+
+        self.set_bitmap_page_refs(page_refs);
     }
 
     fn initialized(&self) -> bool {
-        !self.bitmap.is_null()
+        self.bitmap.is_some()
+    }
+
+    fn read_bitmap_u64(&self, index: usize) -> u64 {
+        if let Some(bitmap) = &self.bitmap {
+            match bitmap {
+                ValidBitmapMem::Ptr(ptr) => unsafe { ptr::read(ptr.add(index)) },
+                ValidBitmapMem::PageRefs(pr) => {
+                    let page_refs = pr.as_slice();
+                    let (idx, off) = (index / PAGE_N64, index % PAGE_N64);
+                    unsafe {
+                        let ptr = page_refs[idx].as_ref() as *const [u8; PAGE_SIZE] as *const u64;
+                        ptr::read(ptr.add(off))
+                    }
+                }
+            }
+        } else {
+            unreachable!("ValidBitmap: No bitmap initialized");
+        }
+    }
+
+    fn write_bitmap_u64(&mut self, index: usize, val: u64) {
+        if let Some(bitmap) = &mut self.bitmap {
+            match bitmap {
+                ValidBitmapMem::Ptr(ptr) => unsafe {
+                    ptr::write(ptr.add(index), val);
+                },
+                ValidBitmapMem::PageRefs(pr) => {
+                    let page_refs = pr.as_mut_slice();
+                    let (idx, off) = (index / PAGE_N64, index % PAGE_N64);
+                    unsafe {
+                        let ptr = page_refs[idx].as_mut() as *mut [u8; PAGE_SIZE] as *mut u64;
+                        ptr::write(ptr.add(off), val);
+                    }
+                }
+            }
+        } else {
+            unreachable!("ValidBitmap: No bitmap initialized");
+        }
     }
 
     fn set_valid_4k(&mut self, paddr: PhysAddr) {
@@ -173,11 +286,8 @@ impl ValidBitmap {
         assert!(paddr.is_page_aligned());
         assert!(self.check_addr(paddr));
 
-        unsafe {
-            let mut val: u64 = ptr::read(self.bitmap.add(index));
-            val |= 1u64 << bit;
-            ptr::write(self.bitmap.add(index), val);
-        }
+        let val = self.read_bitmap_u64(index) | (1u64 << bit);
+        self.write_bitmap_u64(index, val);
     }
 
     fn clear_valid_4k(&mut self, paddr: PhysAddr) {
@@ -190,11 +300,8 @@ impl ValidBitmap {
         assert!(paddr.is_page_aligned());
         assert!(self.check_addr(paddr));
 
-        unsafe {
-            let mut val: u64 = ptr::read(self.bitmap.add(index));
-            val &= !(1u64 << bit);
-            ptr::write(self.bitmap.add(index), val);
-        }
+        let val = self.read_bitmap_u64(index) & !(1u64 << bit);
+        self.write_bitmap_u64(index, val);
     }
 
     fn set_2m(&mut self, paddr: PhysAddr, val: u64) {
@@ -202,16 +309,14 @@ impl ValidBitmap {
             return;
         }
 
-        const NR_INDEX: usize = PAGE_SIZE_2M / (PAGE_SIZE * 64);
+        const NR_INDEX: usize = PAGE_SIZE_2M / (PAGE_SIZE * u64::BITS as usize);
         let (index, _) = self.index(paddr);
 
         assert!(paddr.is_aligned(PAGE_SIZE_2M));
         assert!(self.check_addr(paddr));
 
         for i in 0..NR_INDEX {
-            unsafe {
-                ptr::write(self.bitmap.add(index + i), val);
-            }
+            self.write_bitmap_u64(index + i, val);
         }
     }
 
@@ -224,9 +329,9 @@ impl ValidBitmap {
     }
 
     fn modify_bitmap_word(&mut self, index: usize, mask: u64, new_val: u64) {
-        let val = unsafe { ptr::read(self.bitmap.add(index)) };
+        let val = self.read_bitmap_u64(index);
         let val = (val & !mask) | (new_val & mask);
-        unsafe { ptr::write(self.bitmap.add(index), val) };
+        self.write_bitmap_u64(index, val);
     }
 
     fn set_range(&mut self, paddr_begin: PhysAddr, paddr_end: PhysAddr, new_val: bool) {
@@ -246,7 +351,7 @@ impl ValidBitmap {
             self.modify_bitmap_word(index_head, mask_head, new_val);
 
             for index in (index_head + 1)..index_tail {
-                unsafe { ptr::write(self.bitmap.add(index), new_val) };
+                self.write_bitmap_u64(index, new_val);
             }
 
             if bit_tail_end != 0 {
@@ -277,12 +382,9 @@ impl ValidBitmap {
 
         assert!(self.check_addr(paddr));
 
-        unsafe {
-            let mask: u64 = 1u64 << bit;
-            let val: u64 = ptr::read(self.bitmap.add(index));
-
-            (val & mask) == mask
-        }
+        let mask = 1u64 << bit;
+        let val = self.read_bitmap_u64(index);
+        (val & mask) == mask
     }
 }
 
